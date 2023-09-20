12cc000a2943376c100a07c672d0b4ac979790d3
Anton Vasylchenko <87824792+Smd16Bi@users.noreply.github.com>
2023-09-20 06:23:52 +0300

    added header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import style from './header.module.css';
+import iconAccount from '../../assets/icon-account.svg';
 
 const Header: React.FC = () => {
-  return <header>Header: React.FC</header>;
+  return (
+    <header className={style.header}>
+      <div className={style.title}>
+        <h1>Weekly sumup</h1>
+        <p>Get summary of your weekly online transactions here.</p>
+      </div>
+      <div className={style.account}>
+        <div className={style.account_img}>
+          <img src={iconAccount} alt="Account" width={35} height={35} loading="lazy" />
+        </div>
+        <div className={style.account_info}>
+          <span className={style.account_name}>Andrew</span>
+          <span className={style.account_type}>Admin account</span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
