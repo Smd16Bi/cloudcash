086d4f38e8f06067b4515d951b871105bb7de647
Anton Vasylchenko <87824792+Smd16Bi@users.noreply.github.com>
2023-09-20 06:37:48 +0300

    added footer

--- src/component/index.js
+++ src/component/index.js
@@ -1,2 +1,3 @@
 export { default as Aside } from "../component/aside/Aside";
 export { default as Header } from "../component/header/Header";
+export { default as Footer } from "../component/footer/Footer";
